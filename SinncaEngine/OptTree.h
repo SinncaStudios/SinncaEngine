165c5a47cd399070152b838abe96dd0b2d9e45f9
SinncaStudios <dracoduo@gmail.com>
2015-06-22 19:38:26 -0400

    Started branch for developing quadtrees and octrees

---
+++ SinncaEngine/OptTree.h
@@ -0,0 +1,27 @@
+//
+//  OptTree.h
+//  SinncaEngine
+//
+//  Created by Ryan Oldis on 6/21/15.
+//  Copyright (c) 2015 Ryan Oldis. All rights reserved.
+//
+
+#ifndef SinncaEngine_OptTree_h
+#define SinncaEngine_OptTree_h
+
+#include "Node.h"
+
+namespace sinnca
+{
+	class optTree
+	{
+		
+		struct leaf : public node
+		{
+			
+		}
+		
+	};
+}
+
+#endif
